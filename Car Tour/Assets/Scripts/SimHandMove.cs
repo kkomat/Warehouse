df82c80e8694e66c8b64c38b8a9f46ac5ca6c126
Karthik Komatineni <komatineni.karthik@gmail.com>
2021-03-01 17:14:11 -0500

    Added Unity Project

---
+++ Car Tour/Assets/Scripts/SimHandMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimHandMove : MonoBehaviour
+{
+    public Transform location;
+    public Vector3 position;
+    public float movespeed;
+    public float turnSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movespeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * movespeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movespeed);
+        }
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * turnSpeed, Space.Self);
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse Y") * turnSpeed, Space.Self);
+
+
+    }
+}
